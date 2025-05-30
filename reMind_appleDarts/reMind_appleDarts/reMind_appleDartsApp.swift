c334b9c4c202fe8e57e50301bdc500e4df815afc
Sumi-Dumi <77911196+Sumi-Dumi@users.noreply.github.com>
2025-05-30 11:25:59 +1000

    firstPush

---
+++ reMind_appleDarts/reMind_appleDarts/reMind_appleDartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  reMind_appleDartsApp.swift
+//  reMind_appleDarts
+//
+//  Created by Sumi on 30/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct reMind_appleDartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
