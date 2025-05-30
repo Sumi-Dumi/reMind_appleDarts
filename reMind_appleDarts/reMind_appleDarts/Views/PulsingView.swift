611e6fe45b6f42e13d8c03475a02bbaef45d7d6a
karanthTanvi <ta.karanth@gmail.com>
2025-05-30 13:33:25 +1000

    new pulsing view

---
+++ reMind_appleDarts/reMind_appleDarts/Views/PulsingView.swift
@@ -0,0 +1,23 @@
+//
+//  PulsingView.swift
+//  
+//
+//  Created by Tanvi on 30/5/2025.
+//
+
+
+
+import SwiftUI
+
+struct PulsingView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    PulsingView()
+}
