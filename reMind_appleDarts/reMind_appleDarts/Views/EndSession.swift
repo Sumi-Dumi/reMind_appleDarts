45eb2b3a16017558add57a5d5df34c12a354e38a
Ansha-code <ansha.101011@gmail.com>
2025-05-30 14:05:13 +1000

    push end session

---
+++ reMind_appleDarts/reMind_appleDarts/Views/EndSession.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  reMind
+//
+//  Created by Ansha on 30/5/2025.
+//
+
+import SwiftUI
+
+struct EndSessionView: View {
+    var body: some View {
+        VStack(spacing: 22){
+            Text("✔️")
+            Text("End of Session")
+                .font(.title)
+            
+            HStack{
+                ZStack{
+                    Rectangle()
+                        .fill(Color.gray)
+                        .frame(width: 100, height: 30)
+                        .cornerRadius(10)
+                    Button(action: {
+                        // action here
+                    }) {
+                        Text("Finish")
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                    }
+                }
+                ZStack{
+                    Rectangle()
+                        .fill(Color.green)
+                        .frame(width: 100, height: 30)
+                        .cornerRadius(10)
+                    Button(action: {
+                        // action here
+                    }) {
+                        Text("Resume")
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                    }
+                }
+                //Button(action: signIn) {
+                //    Text("Sign In")
+                // }
+                
+            }
+        
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    EndSessionView()
+}
